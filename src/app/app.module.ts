b62b86ab51d7d41aa427e7afd9c13e065b275b7b
Sanjay Shirodkar <49332574+sanshirodkar@users.noreply.github.com>
2020-02-05 10:37:05 +0900

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppBoostrapModule } from './app-bootstrap/app-bootstrap.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppBoostrapModule ],
-  declarations: [ AppComponent, ServerComponent, ServersComponent ],
+  declarations: [ 
+    AppComponent, ServerComponent, ServersComponent, 
+    SuccessAlertComponent, WarningAlertComponent 
+   ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
